544d1dbc0b617897247cd6a12a58c6a740d3a15d
SoumyaKodumuri <soumyakodumuri6@gmail.com>
2024-07-25 09:31:03 +0530

    Create GreaterOrLesser.java

---
+++ GreaterOrLesser.java
@@ -0,0 +1,27 @@
+public class GreaterOrLesser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10, b=40, c=30;
+		if(a>b)
+		{
+			if(a>c) {
+				System.out.println("a is greater");
+			}
+			else {
+				System.out.println("c is greater");
+			}
+		}
+		else{
+			if(b>c)
+			{
+			System.out.println("b is greater");
+		    }
+		   else
+		   {
+			System.out.println("c is greater");
+		    }
+	}
+	}
+}

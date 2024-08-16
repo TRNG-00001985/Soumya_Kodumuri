51188290a0cd2f6fb87890f7b3e5dee2085bf0ea
SoumyaKodumuri <soumyakodumuri6@gmail.com>
2024-08-16 14:09:42 +0530

    Create fizzbizz-factorial.js

---
+++ fizzbizz-factorial.js
@@ -0,0 +1,27 @@
+// FIZZBIZZ CODE
+a=15;
+        if((a%3==0) && (a%5==0))
+           {
+            console.log("FizzBizz");
+        }
+        else if(a%3==0) {
+            console.log("Fizz");
+        }
+        else if(a%5==0) {
+            console.log("Bizz");
+        }
+        else {
+            console.log(a);
+        }
+OUTPUT : FizzBizz
+
+//FACTORIAL CODE
+let n=3;
+let factorial = 1;
+for(i=1; i<=n; i++)
+    {
+        factorial=factorial*i;
+    }
+console.log(factorial)
+OUTPUT : 6
+
